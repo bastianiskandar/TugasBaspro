d51a04922e1004bd733a37f7434fb6f6715d8e66
bastianiskandar <135602771+bastianiskandar@users.noreply.github.com>
2025-02-27 11:46:34 +0700

    Add files via upload

---
+++ tugassegitiga.py
@@ -0,0 +1,6 @@
+a= float(input("Masukkan nilai Alas: "))
+t= float(input("Masukkan nilai Tinggi: "))
+
+result = float(a*t/2)
+
+print("Luasnya adalah :", int(result))
---
+++ volumetabung.py
@@ -0,0 +1,6 @@
+r = float(input("Masukkan Jari-jari: "))
+t = float(input("Masukkan Tinggi: "))
+
+hasil = float(3.14*r*r*t)
+
+print("Volume tabung nya adalah: ", hasil)
